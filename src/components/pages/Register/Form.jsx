0b118d8282d8ec21b3fe6ac76e9bd50ebc3981ed
Your Name <you@example.com>
2025-07-18 16:39:32 +0300

    tmp

--- src/components/pages/Register/Form.jsx
+++ src/components/pages/Register/Form.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import PhoneInput from '../../PhoneInput';
-import EmailInput from '../../EmailInput';
-import PasswordInput from '../../PasswordInput';
-import PasswordConfirmationInput from '../../PasswordConfirmationInput';
+import PhoneInput from "../../PhoneInput";
+import EmailInput from "../../EmailInput";
+import NameInput from "../../NameInput";
+import PasswordInput from "../../PasswordInput";
+import PasswordConfirmationInput from "../../PasswordConfirmationInput";
 import Button from "../../Button";
+import CheckboxInput from "../../CheckboxInput";
 
 function Form() {
   return (
@@ -12,24 +14,8 @@ function Form() {
       method="post"
       className="row g-3 mt-2 col-12 col-md-6 mx-auto"
     >
-      <div>
-        <label htmlFor="validationServerName" className="form-label">
-          Имя (кириллица, пробел, дефис)
-        </label>
-        <input
-          type="text"
-          className="form-control is-invalid"
-          id="validationServerName"
-          aria-describedby="nameError"
-          name="name"
-          pattern="[А-Яа-яЁё\s\-]+"
-          required=""
-        />
-        <div id="nameError" className="invalid-feedback">
-          Введите корректное имя.
-        </div>
-      </div>
-      
+      <NameInput />
+
       <PhoneInput />
 
       <EmailInput />
@@ -38,25 +24,13 @@ function Form() {
 
       <PasswordConfirmationInput />
 
-      <div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            defaultValue=""
-            id="validationServerConfirm"
-            aria-describedby="confirmError"
-            name="confirm"
-            required=""
-          />
-          <label className="form-check-label" htmlFor="validationServerConfirm">
-            Согласие на обработку персональных данных.
-          </label>
-          <div id="confirmError" className="invalid-feedback">
-            Необходимо согласиться.
-          </div>
-        </div>
-      </div>
+      <CheckboxInput
+        name="confirm"
+        label="Согласие на обработку персональных данных"
+        errorMessage="Необходимо согласиться."
+        isRequired={true}
+      />
+
       <div className="col-12">
         <Button btnText="Зарегистрироваться" />
       </div>
