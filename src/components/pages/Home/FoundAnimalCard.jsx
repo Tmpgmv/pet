1c20341a9092a1c1684c06e18b8bcacc78c18128
Your Name <you@example.com>
2025-07-19 15:35:59 +0300

    tmp

--- src/components/pages/Home/FoundAnimalCard.jsx
+++ src/components/pages/Home/FoundAnimalCard.jsx
@@ -5,32 +5,31 @@ import Button from "../../Button";
 
 function FoundAnimalCard({ kind, src, alt, date, district }) {
   return (
-    <Link to="/card">
-      <div className="col">
-        <div className="card mb-4 rounded-3 shadow-sm border-primary border-solid">
-          <div className="card-header py-3 border-primary bs-primary-bg-subtle">
-            <h4 className="my-0 fw-normal">{kind}</h4>
+    <div className="col">
+      <div className="card mb-4 rounded-3 shadow-sm border-primary border-solid">
+        <div className="card-header py-3 border-primary bs-primary-bg-subtle">
+          <h4 className="my-0 fw-normal">{kind}</h4>
+        </div>
+        <div className="card-body">
+          <img className="w-100" src={src} alt={alt} />
+          <div className="row mt-2">
+            <div className="w-25 text-end">
+              <img src={calendar} height={30} alt="calendar" />
+            </div>
+            <div className="w-75 my-auto text-start fw-bold">{date}</div>
           </div>
-          <div className="card-body">
-            <img className="w-100" src={src} alt={alt} />
-            <div className="row mt-2">
-              <div className="w-25 text-end">
-                <img src={calendar} height={30} alt="calendar" />
-              </div>
-              <div className="w-75 my-auto text-start fw-bold">{date}</div>
+          <div className="row mb-2 mh-3em">
+            <div className="w-25 text-end">
+              <img src={geo} height={30} alt="geo" />
             </div>
-            <div className="row mb-2 mh-3em">
-              <div className="w-25 text-end">
-                <img src={geo} height={30} alt="geo" />
-              </div>
-              <div className="w-75 my-auto text-start fw-bold ">{district}</div>
-            </div>
-
+            <div className="w-75 my-auto text-start fw-bold ">{district}</div>
+          </div>
+          <Link to="/card">
             <Button btnText="Подробнее" aClassName="w-100" />
-          </div>
+          </Link>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
 
--- src/components/pages/Search/Card.jsx
+++ src/components/pages/Search/Card.jsx
@@ -6,37 +6,36 @@ import Img from "../../Img";
 
 function Card({ h2, src }) {
   return (
-    <Link to="/card">
-      <div className="col">
-        <div className="card mb-4 rounded-3 shadow-sm border-primary border-solid">
-          <div className="card-header py-3 border-primary bs-primary-bg-subtle">
-            <h2 className="my-0 fw-normal">{h2}</h2>
+    <div className="col">
+      <div className="card mb-4 rounded-3 shadow-sm border-primary border-solid">
+        <div className="card-header py-3 border-primary bs-primary-bg-subtle">
+          <h2 className="my-0 fw-normal">{h2}</h2>
+        </div>
+        <div className="card-body">
+          <Img src={src} aClassName="w-100" alt="pet" />
+
+          <div className="row mt-2">
+            <div className="w-25 text-end">
+              <Img src={calendar} height={30} alt="calendar" />
+            </div>
+            <div className="w-75 text-start my-auto fw-bold">
+              30 июня 2025 г.
+            </div>
           </div>
-          <div className="card-body">
-            <Img src={src} aClassName="w-100" alt="pet" />
-
-            <div className="row mt-2">
-              <div className="w-25 text-end">
-                <Img src={calendar} height={30} alt="calendar" />
-              </div>
-              <div className="w-75 text-start my-auto fw-bold">
-                30 июня 2025 г.
-              </div>
+          <div className="row mb-2 mh-3em">
+            <div className="w-25 text-end my-auto">
+              <Img src={geo} height={30} alt="geo" />
             </div>
-            <div className="row mb-2 mh-3em">
-              <div className="w-25 text-end my-auto">
-                <Img src={geo} height={30} alt="geo" />
-              </div>
-              <div className="w-75 text-start my-auto fw-bold ">
-                Красногвардейский район
-              </div>
+            <div className="w-75 text-start my-auto fw-bold ">
+              Красногвардейский район
             </div>
-
+          </div>
+          <Link to="/card">
             <Button aClassName="w-100" btnText="Подробнее" />
-          </div>
+          </Link>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
 
