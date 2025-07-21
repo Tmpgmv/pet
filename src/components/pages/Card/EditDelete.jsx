185e3fabc458a87be5f447ce14c558da12162e7f
Your Name <you@example.com>
2025-07-21 16:36:25 +0300

    tmp

--- src/components/pages/Card/EditDelete.jsx
+++ src/components/pages/Card/EditDelete.jsx
@@ -5,8 +5,8 @@ function EditDelete() {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center">
       <div className="d-inline-flex ms-md-auto">
-        <FormSingleAction id="edit" />
-        <FormSingleAction id="delete" />        
+        <FormSingleAction id="edit" inputClassName="ps-3 pe-3"/>
+        <FormSingleAction id="delete" inputClassName="ps-3 pe-3"/>        
       </div>
     </div>
   );
