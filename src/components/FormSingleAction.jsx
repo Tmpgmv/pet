935ddcf189617dccfe48e1a448cc4195a49b871f
Your Name <you@example.com>
2025-07-22 16:49:18 +0300

    tmp

--- src/components/FormSingleAction.jsx
+++ src/components/FormSingleAction.jsx
@@ -1,7 +1,7 @@
 
 
 function FormSingleAction({id, formClassName=null, inputClassName=null, onSubmit=null}) {
-    debugger;
+    
     return (
         <form id={id} method="post" className={formClassName ?? undefined} onSubmit={onSubmit ?? undefined}>
             <input className={`${id} ${inputClassName}`} type="submit" value="" />
