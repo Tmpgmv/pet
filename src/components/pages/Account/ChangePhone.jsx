732316fb10097cbe52d77640dc52b67bee90bc2a
Your Name <you@example.com>
2025-07-23 18:28:08 +0300

    tmp

--- src/components/pages/Account/ChangePhone.jsx
+++ src/components/pages/Account/ChangePhone.jsx
@@ -1,8 +1,73 @@
 import Button from "../../Button";
 import H2 from "../../H2";
 import PhoneInput from "../../PhoneInput";
+import { ACCOUNT, API_CHANGE_PHONE_URL } from "../../../general/constants";
+import getToken from "../../../general/getToken";
+import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import $ from "jquery";
+import { toast } from "react-toastify";
+
+
+function clear() {
+  $(".is-valid, .is-invalid").removeClass("is-valid is-invalid");
+}
 
 function ChangePhone() {
+
+  const location = useLocation();
+  const TOKEN = getToken();
+  const FORM_ID = "change-phone-form";
+  const navigate = useNavigate();
+  const notifyFailure = () => toast.error("Не удалось изменить телефон!");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    clear();
+
+    let formData = $("#" + FORM_ID).serialize();
+
+    let request = $.ajax({
+      url: API_CHANGE_PHONE_URL,
+      method: "PATCH",
+      data: formData,
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader("Authorization", "Bearer " + TOKEN);
+      },
+      dataType: "json",
+    });
+
+    request.done(function (data, textStatus, jqXHR) {
+            navigate(ACCOUNT, {
+              state: {
+                toast: {
+                  type: "success",
+                  message: "Телефон изменен.",
+                },
+                from: location,
+              },
+            });
+    });
+
+    request.fail(function (jqXHR, textStatus, errorThrown) {
+      notifyFailure();
+
+      let responseText = jqXHR.responseText;
+
+      if (responseText) {
+        let responseTextJson = $.parseJSON(responseText);
+        let errors = responseTextJson.error.error;
+
+        $.each(errors, function (key, data) {
+          let unitedErrorText = data.join();
+          $("#validationServerPhone").addClass("is-invalid");
+          let selector = "#" + key + "Error";
+          $(selector).text(unitedErrorText);
+        });
+      }
+    });
+  }
+
   return (
     <section id="change-phone" className="mt-5">
       <H2 h2="Изменить телефон" />
@@ -11,6 +76,7 @@ function ChangePhone() {
         id="change-phone-form"
         className="mx-auto col col-md-8 mt-4"
         method="post"
+        onSubmit={(event) => handleSubmit(event)}
       >
         <PhoneInput />
 
--- src/general/constants.js
+++ src/general/constants.js
@@ -1,6 +1,7 @@
 export const LOGIN = "/login";
 export const ACCOUNT = "/account";
 export const CHANGE_EMAIL = "/email";
+export const CHANGE_PHONE = "/phone";
 
 export const API_URL = "https://pets.сделай.site/api";
 export const API_URL_REGISTRATION_PATH = API_URL + "/register";
@@ -9,3 +10,4 @@ export const API_URL_LOGIN_PATH = API_URL + "/login";
 export const API_URL_USERS_PATH = API_URL + "/users";
 export const API_URL_USERS_POSTS = API_URL_USERS_PATH +"/orders"; // Объявления, добавленные пользователем.
 export const API_CHANGE_EMAIL_URL = API_URL_USERS_PATH + CHANGE_EMAIL;
+export const API_CHANGE_PHONE_URL = API_URL_USERS_PATH + CHANGE_PHONE;
